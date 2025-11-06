65d3e8ef4b58bc35eccf6d7c4cda74dae47e333c
SATHWIIKK31 <sathwiikk@gmail.com>
2025-11-06 18:18:34 +0530

    connected to database

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./db/index.js";
+dotenv.config({
+    path:'./.env'
+});
+const app=express();
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error",(error)=>{
+            console.error("Error from the server side", error);
+        });
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port : ${process.env.PORT}`);
+        });
+        
+
+    }
+    catch(error){
+        console.error("Error connecting to the database", error);
+
+    }
+
+})()
+*/
